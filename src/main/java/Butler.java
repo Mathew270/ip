e89e85607f45f27d054173ceb7699ed997f7e2a2
Mathew <Mathewpaul2004@gmail.com>
2025-08-20 14:06:00 +0800

    Completed Level-3
    mark, unmark tasks
    created Task class

--- src/main/java/Butler.java
+++ src/main/java/Butler.java
@@ -9,26 +9,40 @@ public class Butler {
         System.out.println(line);
 
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
-            if (!sc.hasNextLine()) {
-                break;
-            }
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon");
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
             } else if (input.equals("list")) {
                 System.out.println(line);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i].toString());
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.substring(5)) - 1;
+                tasks[idx].mark();
+                System.out.println(line);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[idx].toString());
+                System.out.println(line);
+            } else if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.substring(7)) - 1;
+                tasks[idx].unmark();
+                System.out.println(line);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[idx].toString());
+                System.out.println(line);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(line);
                 System.out.println(" added: " + input);
