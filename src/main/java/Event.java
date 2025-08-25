094655780771c29fcefe9136062853508a120c59
Mathew <Mathewpaul2004@gmail.com>
2025-08-25 16:45:19 +0800

    Update Deadline, Event to use LocalDate/Time
    currently does not display dates/time in a user firendly manner
    
    the proposed changes make date/times more readable to the user
    by making use of java.time.format.DateTimeFormatter

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,17 @@
-public class Event extends Task {
-    private final String from;
-    private final String to;
+import java.time.LocalDateTime;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    private final LocalDateTime from;
+    private final LocalDateTime to;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public String getFrom() { return from; }
-    public String getTo() { return to; }
+    public LocalDateTime getFrom() { return from; }
+    public LocalDateTime getTo() { return to; }
 
     @Override
     public String typeIcon() { return "[E]"; }
@@ -19,19 +21,22 @@ public class Event extends Task {
 
     @Override
     public String serialize() {
+        // Persist in ISO-8601 (LocalDateTime#toString -> 2025-08-25T18:00)
         return String.join("|",
                 typeCode(),
                 isDone ? "1" : "0",
                 description,
-                from,
-                to
+                from.toString(),
+                to.toString()
         );
     }
 
     @Override
     public String toString() {
         return typeIcon() + statusIcon() + " " + description
-                + " (from: " + from + " to: " + to + ")";
+                + " (from: " + from.format(DISPLAY_DATETIME)
+                + " to: " + to.format(DISPLAY_DATETIME) + ")";
     }
 }
+
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,12 @@
+import java.time.format.DateTimeFormatter;
+
 public abstract class Task {
     protected final String description;
     protected boolean isDone;
+
+    // For display (subclasses may also use their own)
+    protected static final DateTimeFormatter DISPLAY_DATE = DateTimeFormatter.ofPattern("MMM d yyyy");
+    protected static final DateTimeFormatter DISPLAY_DATETIME = DateTimeFormatter.ofPattern("MMM d yyyy HH:mm");
 
     public Task(String description) {
         this.description = description;
@@ -10,24 +16,17 @@ public abstract class Task {
     public void mark() { this.isDone = true; }
     public void unmark() { this.isDone = false; }
 
-    protected String statusIcon() {
-        return isDone ? "[X]" : "[ ]";
-    }
-
-    public String getDescription() { return description; }
+    protected String statusIcon() { return isDone ? "[X]" : "[ ]"; }
     public boolean isDone() { return isDone; }
+    public String getDescription() { return description; }
 
-    // Polymorphic type code (for file format)
-    public abstract String typeCode();
-
-    // Serialize to line
-    public abstract String serialize();
+    // Polymorphic hooks for persistence/printing
+    public abstract String typeIcon();     // "[T]", "[D]", "[E]"
+    public abstract String typeCode();     // "T", "D", "E"
+    public abstract String serialize();    // e.g. T|1|read book
 
     @Override
     public String toString() {
         return typeIcon() + statusIcon() + " " + description;
     }
-
-    // For subclasses to provide icon
-    public abstract String typeIcon();
 }
